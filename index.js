b11067badd7c06fb81a8d3b00c90c4af4201b968
Umar Farooq <70604902+geekyumar@users.noreply.github.com>
2025-07-22 13:30:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Node.js! This is a added messsage that got updated from CI/CD!\n');
+  res.end('Hello from Node.js! This is a added messsage that got updated from CI/CD, and that works again!\n');
 });
 
 server.listen(80, () => {
